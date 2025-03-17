1d232fefcfa68c80c2ae6954e22ea08f33f973e9
avishekSPC <avishekr@spc.int>
2025-03-17 15:22:03 +1200

    minor changes

--- obj/Debug/net8.0/USPEducation.AssemblyInfo.cs
+++ obj/Debug/net8.0/USPEducation.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("USPEducation")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3e11855096ffeffa317c341534517834f9fc27c8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7c003cdf0bdab7ee1bee6b92640ed355dffdd46a")]
 [assembly: System.Reflection.AssemblyProductAttribute("USPEducation")]
 [assembly: System.Reflection.AssemblyTitleAttribute("USPEducation")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
